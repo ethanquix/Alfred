635bc2d2c00c4f0adec821b6d8c9bd31784b591a
Dimitri <dimitriwyzlic@gmail.com>
2017-05-04 03:29:57 +0200

    [ADDED] .gitignore and starting Logger

---
+++ src/Logger/Logger.cpp
@@ -0,0 +1,11 @@
+/*
+** Logger.cpp for Alfred in /home/wyzlic_a/delivery/Alfred/Logger.cpp
+**
+** Made by Dimitri Wyzlic
+** Login   <dimitri1.wyzlic@epitech.eu>
+**
+** Started on  Thu May 04 03:26:18 2017 Dimitri Wyzlic
+** Last update Thu May 04 03:26:18 2017 Dimitri Wyzlic
+*/
+
+#include "Logger.hpp"
---
+++ src/Logger/Logger.hpp
@@ -0,0 +1,21 @@
+/*
+** Logger.hpp for Alfred in /home/wyzlic_a/delivery/Alfred/Logger.hpp
+**
+** Made by Dimitri Wyzlic
+** Login   <dimitri1.wyzlic@epitech.eu>
+**
+** Started on  Thu May 04 03:26:19 2017 Dimitri Wyzlic
+** Last update Thu May 04 03:26:19 2017 Dimitri Wyzlic
+*/
+
+#ifndef ALFRED_LOGGER_HPP
+#define ALFRED_LOGGER_HPP
+
+namespace Alfred
+{
+    class Logger
+    {
+    };
+}
+
+#endif //ALFRED_LOGGER_HPP
